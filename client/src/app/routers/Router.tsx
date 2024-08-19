cd385312eceac12d1cc2d632766bf05bd876b4c8
ArtiomKochkin <artiom.kochkin@yandex.ru>
2024-08-19 19:35:10 +0700

    upd: add lazy load

--- client/src/app/routers/Router.tsx
+++ client/src/app/routers/Router.tsx
@@ -1,15 +1,22 @@
-import { ChatPage } from "@pages/ChatPage";
-import { HomePage } from "@pages/HomePage";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+const HomePage = lazy(() =>
+  import('@pages/HomePage').then(module => ({ default: module.HomePage }))
+);
+const ChatPage = lazy(() => 
+  import('@pages/ChatPage').then(module => ({ default: module.ChatPage}))
+);
 
 export const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/chat" element={<ChatPage/>}/>
-        <Route path="/" element={<HomePage/>}/>
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<ChatPage/>}/>
+          <Route path="/" element={<HomePage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   )
 }
